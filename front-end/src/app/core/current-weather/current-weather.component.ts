6ba20fe8839c4dbe4a5c3efe69adc624a7a98090
kickstyl3 <34608363+kickstyl3@users.noreply.github.com>
2020-12-12 18:47:54 +0200

    feat: add default city on ngOnInit

--- front-end/src/app/core/current-weather/current-weather.component.ts
+++ front-end/src/app/core/current-weather/current-weather.component.ts
@@ -7,9 +7,9 @@ import { WeatherService } from '../weather.service';
   styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent implements OnInit {
+  city: string;
   description: string;
   temperature: number;
-  city: string;
   cityName: string;
 
   constructor(
@@ -18,11 +18,48 @@ export class CurrentWeatherComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.cityName = 'London';
+
+    this.weatherService
+      .getCurrentWeather(this.cityName)
+      .subscribe({
+        next: (data) => {
+          const { main, name, sys, visibility, weather, wind } = data;
+          // console.log(main, name, sys, visibility, weather, wind);
+          const [description] = weather;
+          const { main: desc } = description;
+          this.description = desc;
+          this.temperature = main.temp.toFixed(0);
+          this.city = this.cityName;
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      })
   }
 
   searchHandler(formValue: { searchedCity: string }) {
     this.cityName = formValue.searchedCity;
-    if (this.cityName !== undefined) {
+    if (this.cityName) {
+      this.weatherService
+        .getCurrentWeather(this.cityName)
+        .subscribe({
+          next: (data) => {
+            const { main, name, sys, visibility, weather, wind } = data;
+            // console.log(main, name, sys, visibility, weather, wind);
+            const [description] = weather;
+            const { main: desc } = description;
+            this.description = desc;
+            this.temperature = main.temp.toFixed(0);
+            this.city = this.cityName;
+          },
+          error: (err) => {
+            console.error(err);
+          }
+        })
+    } else {
+      this.cityName = 'London';
+
       this.weatherService
         .getCurrentWeather(this.cityName)
         .subscribe({
