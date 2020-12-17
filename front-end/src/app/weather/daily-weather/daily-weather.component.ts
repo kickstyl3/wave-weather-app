5d101c48f3418a1b894742ba1da5fbe4a0ee8695
kickstyl3 <34608363+kickstyl3@users.noreply.github.com>
2020-12-17 02:21:05 +0200

    feat: modify daily weather page + add icons

--- front-end/src/app/weather/daily-weather/daily-weather.component.ts
+++ front-end/src/app/weather/daily-weather/daily-weather.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, DoCheck } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../weather.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-daily-weather',
   templateUrl: './daily-weather.component.html',
   styleUrls: ['./daily-weather.component.css']
 })
-export class DailyWeatherComponent implements OnInit, DoCheck {
+export class DailyWeatherComponent implements OnInit {
   dailyData;
 
   constructor(
@@ -20,7 +21,8 @@ export class DailyWeatherComponent implements OnInit, DoCheck {
         .subscribe({
           next: (data) => {
             data.daily.shift();
-            const splicedData = data.daily.splice(4, 7);
+            const splicedData = data.daily.splice(6, 8);
+            data.daily.shift();
 
             this.dailyData = data.daily;
             console.log(this.dailyData);
@@ -32,7 +34,10 @@ export class DailyWeatherComponent implements OnInit, DoCheck {
     // }, 5000)
   }
 
-  ngDoCheck(): void {
+  formatDay(unixTime) {
+    const day = moment.unix(unixTime).format("dddd");
 
+    return day;
   }
+  
 }
